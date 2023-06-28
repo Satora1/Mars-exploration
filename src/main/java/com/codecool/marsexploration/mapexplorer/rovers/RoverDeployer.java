cbfdde8ed1e0638f8421065b71fc74c6884d8c6b
TomekGalazka <tom3k.galazka@gmail.com>
2023-06-28 09:44:18 +0200

    Implement buildings

--- src/main/java/com/codecool/marsexploration/mapexplorer/rovers/RoverDeployer.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/rovers/RoverDeployer.java
@@ -1,6 +1,7 @@
 package com.codecool.marsexploration.mapexplorer.rovers;
 
 import com.codecool.marsexploration.mapexplorer.Configuration.Config;
+import com.codecool.marsexploration.mapexplorer.buildings.Building;
 import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
 import com.codecool.marsexploration.mapexplorer.maploader.model.Map;
 
@@ -18,6 +19,20 @@ public class RoverDeployer {
                 new Coordinate(config.landingCoordinates().X(), config.landingCoordinates().Y() + 1),
                 new Coordinate(config.landingCoordinates().X() - 1, config.landingCoordinates().Y()),
                 new Coordinate(config.landingCoordinates().X() + 1, config.landingCoordinates().Y())
+        };
+
+        Optional<Coordinate> roverCoordinate = Arrays.stream(adjacentCoordinatesOfStarship)
+                .filter(map::isEmpty).findFirst();
+
+        rover.setCurrentPosition(roverCoordinate.orElseThrow(() -> new IllegalStateException("Coordinate not present")));
+    }
+
+    public void placeRover(MarsRover rover, Map map, Building building) {
+        Coordinate[] adjacentCoordinatesOfStarship = {
+                new Coordinate(building.getBaseCoordinate().X(), building.getBaseCoordinate().Y() - 1),
+                new Coordinate(building.getBaseCoordinate().X(), building.getBaseCoordinate().Y() + 1),
+                new Coordinate(building.getBaseCoordinate().X() - 1, building.getBaseCoordinate().Y()),
+                new Coordinate(building.getBaseCoordinate().X() + 1, building.getBaseCoordinate().Y())
         };
 
         Optional<Coordinate> roverCoordinate = Arrays.stream(adjacentCoordinatesOfStarship)
